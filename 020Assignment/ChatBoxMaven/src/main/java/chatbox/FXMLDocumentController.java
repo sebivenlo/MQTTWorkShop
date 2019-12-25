022c5335f5f2d83e2a5ebea543b0ebe1e986ea16
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2019-12-25 12:39:41 +0100

    formatting

--- 020Assignment/ChatBoxMaven/src/main/java/chatbox/FXMLDocumentController.java
+++ 020Assignment/ChatBoxMaven/src/main/java/chatbox/FXMLDocumentController.java
@@ -22,22 +22,22 @@ public class FXMLDocumentController implements Initializable, ActionListener {
 
     @FXML
     Button btnSend;
-    
+
     @FXML
     Button btnConnect;
-    
+
     @FXML
     Button btnDisconnect;
-    
+
     @FXML
     Button btnJoin;
-    
+
     @FXML
     TextField tbTopic;
-    
+
     @FXML
     TextField tbUserId;
-    
+
     @FXML
     TextField tbIpAdress;
 
@@ -52,76 +52,94 @@ public class FXMLDocumentController implements Initializable, ActionListener {
 
     PAHOClient client;
     ChatCallback chatCallBack;
-    
-    
+
     /**
-     * Connect to the PAHOClient which you just implemented. 
+     * connect to the PAHOClient which you just implemented.
+     *
      * @param event event is being parsed by the GUI.
-     * use tbIpAdress and tbUserId from the gui to initialize the client.
-     * check if IpAdress is empty before trying to connect. (prevent crash).
+     *              use tbIpAdress and tbUserId from the gui to initialize the client.
+     *              check if IpAdress is empty before trying to connect. (prevent crash).
      */
     @FXML
-    private void Connect(ActionEvent event) {
+    private void connect( ActionEvent event ) {
         //TODO 2.0 implement connect. 
-        switchButtons(true);
+        String ipAdress = tbIpAdress.getText();
+        String clientId = tbUserId.getText();
+        client = new PAHOClient( ipAdress, clientId );
+        switchButtons( true );
     }
-    
+
     /**
-     * Disconnect from the PAHOClient.
-     * @param event event is being parsed by the GUI. 
+     * dicconnect from the PAHOClient.
+     *
+     * @param event event is being parsed by the GUI.
      */
     @FXML
-    private void Disconnect(ActionEvent event) {
+    private void disConnect( ActionEvent event ) {
         //TODO 2.1 same as in the previous disconnect just call the method.
-        switchButtons(false);
+        client.dicconnect();
+        switchButtons( false );
     }
-    
+
     /**
-     * Subscribe to a Topic.
+     * subscribe to a Topic.
+     *
      * @param event event is being parsed by the GUI.
-     * Subscribe to the topic that is set in the gui. This is stored in the tbTopic variable.
-     * Just like with the connect implementation, please check if the topic is filled before subscribing to null.
+              subscribe to the topic that is set in the gui. This is stored in the
+              tbTopic variable.
+              Just like with the connect implementation, please check if the topic is
+              filled before subscribing to null.
      */
     @FXML
-    private void Subscribe(ActionEvent event) {
+    private void subscribe( ActionEvent event ) {
         //TODO 2.2 implement subscribe
+        String topic = tbTopic.getText();
+        chatCallBack = new ChatCallback();
+        chatCallBack.addListener( this );
+        client.subscribe( topic, chatCallBack );
     }
-    
+
     /**
-     * Publish to an topic.
+     * publish to an topic.
+     *
      * @param event event is being parsed by the GUI.
-     * publish to the topic entered. 
-     * make sure the text in tbMessage and tbTopic is not empty.
+     *              publish to the topic entered.
+     *              make sure the text in tbMessage and tbTopic is not empty.
      */
     @FXML
-    private void Publish(ActionEvent event) {
+    private void publish( ActionEvent event ) {
         //TODO 2.3 implement publish
-        
+        String messagetext = tbMessage.getText();
+        String topic = tbTopic.getText();
+        client.publish( topic, messagetext );
+
     }
+
     /**
      * function disabling buttons whenever needed.
-     * @param enabled 
+     *
+     * @param enabled
      */
-    private void switchButtons(boolean enabled ) {
-        this.btnConnect.disableProperty().set(enabled);
-        this.btnDisconnect.disableProperty().set(!enabled);
-        this.btnSend.disableProperty().set(!enabled);
-        this.btnJoin.disableProperty().set(!enabled);
+    private void switchButtons( boolean enabled ) {
+        this.btnConnect.disableProperty().set( enabled );
+        this.btnDisconnect.disableProperty().set( !enabled );
+        this.btnSend.disableProperty().set( !enabled );
+        this.btnJoin.disableProperty().set( !enabled );
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        switchButtons(false);
+    public void initialize( URL url, ResourceBundle rb ) {
+        switchButtons( false );
     }
 
     @Override
-    public void actionPerformed(java.awt.event.ActionEvent e) {
-        MessageEvent ev = (MessageEvent) e;
+    public void actionPerformed( java.awt.event.ActionEvent e ) {
+        MessageEvent ev = ( MessageEvent ) e;
 
-        Platform.runLater(new Runnable() {
+        Platform.runLater( new Runnable() {
             @Override
             public void run() {
-                lvChatLog.getItems().add(ev.topic + " : " + ev.message);
+                lvChatLog.getItems().add( ev.topic + " : " + ev.message );
             }
         }
         );
--- 020Assignment/ChatBoxMaven/src/main/java/chatbox/PAHOClient.java
+++ 020Assignment/ChatBoxMaven/src/main/java/chatbox/PAHOClient.java
@@ -1,11 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chatbox;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
@@ -18,7 +16,7 @@ public class PAHOClient {
     int qos = 2;
     String ipAdress, clientId;
     MemoryPersistence persistence = new MemoryPersistence();
-    MqttClient Client;
+    MqttClient client;
 
     /**
      * Constructor for the MQTTClient. the MqttClient should be initialized
@@ -26,55 +24,87 @@ public class PAHOClient {
      * ipAdress should be prefixed with "tcp://" and has to connect to port
      * 1883. (if you want to connect to a different host it might be possible
      * that the port has to change as well!)
+     *
      * @param ipAdress ip address the client should connect to
      * @param clientID the name the user gets.
      */
-    public PAHOClient(String ipAdress, String clientID) {
-
-        //TODO 1.0 implement Constructor
+    public PAHOClient( String ipAdress, String clientID ) {
+        this.ipAdress = "tcp://" + ipAdress + ":1883";
+        this.clientId = clientID;
+        try {
+            //TODO 1.0 implement Constructor
+            client = new MqttClient( this.ipAdress, clientID, persistence );
+        } catch ( MqttException ex ) {
+            Logger.getLogger( PAHOClient.class.getName() ).log( Level.SEVERE, null, ex );
+        }
     }
 
     /**
-     * Connect to the Client. a connection needs connection options Tip : create
+     * connect to the client. a connection needs connection options Tip : create
      * a clean session ( using the MqttConnectOptions )
      */
-    public void Connect() {
-        //TODO 1.1 connect to the client
-
+    public void connect() {
+        try {
+            //TODO 1.1 connect to the client
+            client.connect();
+        } catch ( MqttException ex ) {
+            Logger.getLogger( PAHOClient.class.getName() ).log( Level.SEVERE, null, ex );
+        }
     }
 
     /**
-     * Disconnect from the Client. This is a straight-forward method. don't over
+     * dicconnect from the client. This is a straight-forward method. don't over
      * think just close the connection.
      */
-    public void Disconnect() {
-        //TODO 1.2 implement Disconnect
+    public void dicconnect() {
+        try {
+            //TODO 1.2 implement dicconnect
+            client.disconnect();
+        } catch ( MqttException ex ) {
+            Logger.getLogger( PAHOClient.class.getName() ).log( Level.SEVERE, null, ex );
+        }
     }
 
     /**
-     * Subscribe to a Topic.
-     * @param topic topic to subscribe to
+     * subscribe to a Topic.
+     *
+     * @param topic    topic to subscribe to
      * @param callBack ChatCallBack, to handle messages that arrive from that
-     * topic. make sure that the client is subscribed the topic. don't forget to
-     * add a callback to that client for new messages.
+     *                 topic. make sure that the client is subscribed the topic. don't forget to
+     *                 add a callback to that client for new messages.
      */
-    public void Subscribe(String topic, ChatCallback callBack) {
-        //TODO 1.3 implement Subscribe
+    public void subscribe( String topic, ChatCallback callBack ) {
+        try {
+            //TODO 1.3 implement subscribe
+            client.setCallback( callBack );
+            client.subscribe( topic );
+        } catch ( MqttException ex ) {
+            Logger.getLogger( PAHOClient.class.getName() ).log( Level.SEVERE, null, ex );
+        }
     }
-    
+
     /**
      * Method to publish a message to a topic.
      *
-     * @param topic group in which the message should be sent.
+     * @param topic       group in which the message should be sent.
      * @param messagetext actual message that should be sent. Please make sure
-     * that your message will look like:
+     *                    that your message will look like:
      * <TOPIC> : <CLIENTID> : <MESSAGE>
      */
-    public void Publish(String topic, String messagetext) {
-        MqttMessage message;
-        //TODO 1.4 Implement Publish   
+    public void publish( String topic, String messagetext ) {
+        try {
+            MqttMessage message= buildMessage( topic, messagetext );
+            //TODO 1.4 Implement publish
+            
+            client.publish( topic ,message);
+        } catch ( MqttException ex ) {
+            Logger.getLogger( PAHOClient.class.getName() ).log( Level.SEVERE, null, ex );
+        }
     }
 
-    
+    MqttMessage buildMessage( String topic, String messagetext ) {
+        MqttMessage message= new MqttMessage((topic+":"+clientId+ ":"+messagetext).getBytes());
+        return message;
+    }
 
 }
